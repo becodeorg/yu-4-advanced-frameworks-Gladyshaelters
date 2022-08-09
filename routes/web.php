e8fcca9edaed9b9016516bf49c75a241817d5abf
Gladys Haelters <haelters_gladys@hotmail.com>
2022-08-09 11:06:33 +0200

    Add route for contact page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\contactController;
 
 Route::get('/', [HomeController::class, 'show'] )->name("home");
 Route::get('about', [AboutController::class, 'show'] )->name("aboutPage");
+Route::get('contact', [ContactController::class, 'show'] )->name("contact");
