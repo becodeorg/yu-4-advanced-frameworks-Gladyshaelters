9f31b8d425c42cf2681bbdd63a45dc434aa5ff5f
Gladys Haelters <haelters_gladys@hotmail.com>
2022-08-09 11:23:49 +0200

    Create route to signup page and fix error in controller

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -8,6 +8,6 @@ class SignupController extends Controller
 {
     public function show()
     {
-        return view("welcome");
+        return view("pages.signup");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\contactController;
+use App\Http\Controllers\SignupController;
 
 Route::get('/', [HomeController::class, 'show'] )->name("home");
 Route::get('about', [AboutController::class, 'show'] )->name("aboutPage");
 Route::get('contact', [ContactController::class, 'show'] )->name("contact");
+Route::get('signup', [SignupController::class, 'show'] )->name("signup");
