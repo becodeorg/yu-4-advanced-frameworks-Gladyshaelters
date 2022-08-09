c9b3d5315886c76203e117c3233d85623f6f8f0a
Gladys Haelters <haelters_gladys@hotmail.com>
2022-08-09 12:52:22 +0200

    Add basic form to sign up page

--- resources/views/pages/signUp.blade.php
+++ resources/views/pages/signUp.blade.php
@@ -6,5 +6,28 @@
     <p>Here you will sign up with a form to create an account on the platform to get acces</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae asperiores et, eveniet illum totam repellendus ab unde ipsum blanditiis, sapiente in consectetur. Autem, impedit nulla magnam quae ipsa recusandae ad.</p>
 
+    <form action="" method="POST" class="">
+        @csrf
+
+        <label class="" for="name">
+            Name
+        </label>
+        <input class="" name="name" id="name" type="name" value="" placeholder="Your Name">
+
+        <label class="" for="email">
+            E-mail address
+        </label>
+        <input class="" name="email" id="email" type="email" value="" placeholder="E-mail address">
+
+        <label class="" for="password">
+            Password
+        </label>
+        <input class="" name="password" id="password" type="password" placeholder="Password">
+
+        <button class="" type="submit">
+            Sign Up
+        </button>
+
+
 </section>
 @endsection
