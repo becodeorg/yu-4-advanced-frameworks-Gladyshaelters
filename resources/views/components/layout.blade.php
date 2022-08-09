09ba21a7f854b9adfb91baa93bfdbeb4563b3c95
Gladys Haelters <haelters_gladys@hotmail.com>
2022-08-09 11:52:31 +0200

    Add some basic styling and link css to layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
     <title>Document</title>
 </head>
 <body>
     @include('components.header')
-
-    @yield('content')
-
+    <div class="content">
+        @yield('content')
+    </div>
     @include('components.footer')
 
 </body>
