52efd3d7dff716ca1f460cc7b5c036c08e7152e7
Gladys Haelters <haelters_gladys@hotmail.com>
2022-08-09 11:03:48 +0200

    Create contact page, contactcontroller and show function

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('components.layout')
+@section('content')
+<section>
+
+    <h1>CONTACT</h1>
+    <p>Here there will come an contact form and contact information</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae asperiores et, eveniet illum totam repellendus ab unde ipsum blanditiis, sapiente in consectetur. Autem, impedit nulla magnam quae ipsa recusandae ad.</p>
+
+</section>
+@endsection
