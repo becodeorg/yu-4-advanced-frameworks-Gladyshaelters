69fbf1f2432e5b05ab6d40e886af6fea8bf34369
Gladys Haelters <haelters_gladys@hotmail.com>
2022-08-09 10:42:06 +0200

    Create About page and link to all components

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,10 @@
+@extends('components.layout')
+@section('content')
+<section>
+
+    <h1>ABOUT</h1>
+    <p>Here there will come an full explaination of app</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae asperiores et, eveniet illum totam repellendus ab unde ipsum blanditiis, sapiente in consectetur. Autem, impedit nulla magnam quae ipsa recusandae ad.</p>
+
+</section>
+@endsection
