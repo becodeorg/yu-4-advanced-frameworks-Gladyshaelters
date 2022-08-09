5fcd7f5fb6451047ae4bf3f177ba13a589e5593b
Gladys Haelters <haelters_gladys@hotmail.com>
2022-08-09 11:16:30 +0200

    Create signup page and add  basic content

---
+++ resources/views/pages/signUp.blade.php
@@ -0,0 +1,10 @@
+@extends('components.layout')
+@section('content')
+<section>
+
+    <h1>Create an account</h1>
+    <p>Here you will sign up with a form to create an account on the platform to get acces</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae asperiores et, eveniet illum totam repellendus ab unde ipsum blanditiis, sapiente in consectetur. Autem, impedit nulla magnam quae ipsa recusandae ad.</p>
+
+</section>
+@endsection
