73c525b4451f5aad6d74c78f0197276d35d9049c
Gladys Haelters <haelters_gladys@hotmail.com>
2022-08-10 08:25:33 +0200

    Add to have featers of the exercise to track progress

--- resources/views/loggedIn/homeLoggedIn.blade.php
+++ resources/views/loggedIn/homeLoggedIn.blade.php
@@ -3,6 +3,14 @@
 <section>
 
     <h1>Hello world!LOGGED IN</h1>
-
+    <p>Extra content to be reached will logged in.</p>
+    <ul>
+        <li>homepage with summary of latest newsfeed, album updates,...</li>
+        <li>profile</li>
+        <li>newsfeed wich can be updated</li>
+        <li>albums</li>
+        <li>stats page</li>
+        <li></li>
+    </ul>
 </section>
 @endsection
