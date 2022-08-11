c8f80f23e3ea41d9f932e61f30aaaed07a937cde
Gladys Haelters <haelters_gladys@hotmail.com>
2022-08-11 12:21:41 +0200

    Writing a draft introduction on the landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,28 @@
 
     <h1>Hello world!</h1>
 
+    <p>This a platform currently under construction.</p>
+    <p>For the time being we will call It <strong>HeCo</strong></p>
+    <p>Referencing to the combination of Health and Connect (until we find a better one will brainstorming</p>
+    <p>The name itself discribes what this platform is for</p>
+    <P>To connect with Patients in a healthcare environment with it's closed ones and caretakers</P>
+    
+    <p>When hospitalized, it can be hard on the patient and its inner circle.
+        It's easy to feel disconnected, lonely, uninformed,.... and due to Covid I have seen this problem grow</p>  
+
+    <p>This Platform should help with that problem, because we know, a big part of recovery and support for loved ones is Mental health</p>
+    <p>through this platform we hope to raise moral and mental strain in hard and trying times and feel more connected </p>
+    <br>
+    <br>
+    <p>Due to the shortness in time to create this project, i will try to create
+        the platform for just one specific patient type, which will be Neonatalogie</p>
+
+    <p>Lets call it Neo HeCo, a branch in the HeCo project.</p>
+
+
+
+
+
+
 </section>
 @endsection
