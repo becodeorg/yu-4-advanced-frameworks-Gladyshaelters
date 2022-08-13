2403dc4f942aeb9befc423b331b697d4b320e4ce
Gladys Haelters <haelters_gladys@hotmail.com>
2022-08-13 21:09:30 +0200

    Adjusting sturcture

--- app/Http/Controllers/LogInController.php
+++ app/Http/Controllers/LogInController.php
@@ -18,6 +18,6 @@ class LogInController extends Controller
         ]);
         
         $user = $request->input('user');
-        return view("pages.loggedIn")->with('user', $user);  
+        return view("loggedIn.loggedIn")->with('user', $user);  
     }
 }
