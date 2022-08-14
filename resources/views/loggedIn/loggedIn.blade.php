dd4a60752ee0c5b1915b5f01145ec54b5bf923fa
Gladys Haelters <haelters_gladys@hotmail.com>
2022-08-14 22:41:51 +0200

    Edit content of loogedin page

--- resources/views/loggedIn/loggedIn.blade.php
+++ resources/views/loggedIn/loggedIn.blade.php
@@ -2,21 +2,9 @@
 @section('content')
 <section>
 
-    <h1>Hello {{ $user }} ! you are logged in</h1>
+    <h1>Delete? for the moment no use for it anymore</h1>
 
-    <p> The navbar here should change and have all the extra options,
-        or maybe an extra dshboard? not sure yet.</p>
-       <p>Extra content to be reached will logged in.</p>
-    <ul>
-        <li>homepage with summary of latest newsfeed, album updates,...</li>
-        <li>profile</li>
-        <li>newsfeed wich can be updated</li>
-        <li>albums</li>
-        <li>stats page</li>
-        <li></li>
-    </ul>
-
-    <h2>Last status/post update</h2>
+   
 
 
     <h3><a href="/posts">See all status updates</a></h3>
