3fbfa662dd0813c2da3c8766a000068b55defe1e
Gladys Haelters <haelters_gladys@hotmail.com>
2022-08-16 11:31:34 +0200

    Add dummy hard coded agenda for presentation

--- resources/views/loggedIn/agenda.blade.php
+++ resources/views/loggedIn/agenda.blade.php
@@ -1,9 +1,72 @@
 @extends('components.loggedInLayout')
 @section('content')
 <section>
-<p>hello world test AGENDA</p>
+    <div class="calendar-month block">
+        <div class="arrow-btn-container">
 
+            <h2 class="titular">august 2022</h2>
 
-
+        </div>
+        <table class="calendar">
+            <thead class="days-week">
+                <tr>
+                    <th>S</th>
+                    <th>M</th>
+                    <th>T</th>
+                    <th>W</th>
+                    <th>R</th>
+                    <th>F</th>
+                    <th>S</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                </tr>
+                <tr>
+                    <td>8</td>
+                    <td>9</td>
+                    <td>10</td>
+                    <td>11</td>
+                    <td>12</td>
+                    <td>13</td>
+                    <td>14</td>
+                </tr>
+                <tr>
+                    <td>15</td>
+                    <td class="today">16</td>
+                    <td>17</td>
+                    <td>18</td>
+                    <td>19</td>
+                    <td>20</td>
+                    <td>21</td>
+                </tr>
+                <tr>
+                    <td>22</td>
+                    <td>23</td>
+                    <td>24</td>
+                    <td>25</td>
+                    <td>26</td>
+                    <td>27</td>
+                    <td>28</td>
+                </tr>
+                <tr>
+                    <td>29</td>
+                    <td>30</td>
+                    <td>31</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </section>
 @endsection
